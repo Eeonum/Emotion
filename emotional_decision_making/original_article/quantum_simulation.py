32fe8cbc36d136b25d00eb63b1ee0d51260652e4
life-elixir <info@life-elixir.com>
2020-05-23 21:29:24 -0700

    Emotional Decision Making

--- emotional_decision_making/original_article/quantum_simulation.py
+++ emotional_decision_making/original_article/quantum_simulation.py
@@ -2,5 +2,21 @@ import numpy as np
 import scipy as sp
 import matplotlib.pyplot as plt
 from qutip import *
+from math import sqrt
 
-def integrate()
+
+def integrate(N, w_ph, f, w_at, a, b, g, psi0, tlist, gamma, solver):
+    # construct the hamiltonian
+
+    H = 0
+    for j in range(N):
+        H += w_ph * f.dag() * f + \
+             0.5 * w_at[j] * (b.dag()[j] * b[j] - a.dag()[j] * a) + \
+             g[j] / sqrt(N) * (f.dag() * a.dag()[j] * b[j] + b.dag()[j] * a[j] * f)
+
+
+    c_op_list =[]
+
+    # evolve and calculate expectation values
+    if solver=="me":
+        result=mesolve(H, psi0, tlist, c_op_list, sz_list)
